7ef75694be8c9b66f8b488ae170e15d7cd13040e
Jason Juhyun Kim <mtinet@hanmail.net>
2023-04-23 14:09:19 +0900

    Update robotArm_AP.py

--- robotArm_AP.py
+++ robotArm_AP.py
@@ -33,71 +33,106 @@ led.off()
 
 #Template HTML
 html = f"""
-    <!DOCTYPE html>
-    <html>
-      <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>IoT robotArm</title>
-        <center><H1>IoT robotArm</h1></center>
-        <style>
-          /* 버튼 디자인 */
-          .button {{
-            border: none;
-            color: white;
-            padding: 16px 32px;
-            text-align: center;
-            text-decoration: none;
-            display: inline-block;
-            font-size: 16px;
-            margin: 4px 2px;
-            transition-duration: 0.4s;
-            cursor: pointer;
-            background-color: #4CAF50;
-            width: 120px;
-            height: 60px;
-            border-radius: 20%;
-            box-shadow: 2px 2px 2px #888888;
-          }}
-          .button:hover {{
-            background-color: #3e8e41;
-          }}
-        </style>
-
-      </head>
-      <body>
-        <center>
-          <form action="./forward">
-            <button class="button" type="submit">Forward</button>
-          </form>
-          <br>
-          <table>
-            <tr>
-              <td>
-                <form action="./left">
-                  <button class="button" type="submit">Left</button>
-                </form>
-              </td>
-              <td>
-                <form action="./stop">
-                  <button class="button" type="submit">Stop</button>
-                </form>
-              </td>
-              <td>
-                <form action="./right">
-                  <button class="button" type="submit">Right</button>
-                </form>
-              </td>
-            </tr>
-          </table>
-          <br>
-          <form action="./back">
-            <button class="button" type="submit">Back</button>
-          </form>
-        </center>
-      </body>
-    </html>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>IoT robotArm</title>
+    <center><H1>IoT robotArm</h1></center>
+    <style>
+      /* 버튼 디자인 */
+      .button {{
+        border: none;
+        color: white;
+        padding: 16px 32px;
+        text-align: center;
+        text-decoration: none;
+        display: inline-block;
+        font-size: 16px;
+        margin: 4px 2px;
+        transition-duration: 0.4s;
+        cursor: pointer;
+        background-color: #4CAF50;
+        width: 120px;
+        height: 60px;
+        border-radius: 20%;
+        box-shadow: 2px 2px 2px #888888;
+      }}
+      .button:hover {{
+        background-color: #3e8e41;
+      }}
+    </style>
+  </head>
+  <body>
+    <center>
+      <table>
+        <tr>
+          <td>
+            <form action="./up">
+              <button class="button" type="submit">Up</button>
+            </form>
+            <br>
+            <table>
+              <tr>
+                <td>
+                  <form action="./ccw">
+                    <button class="button" type="submit">CCW</button>
+                  </form>
+                </td>
+                <td>
+                  <form action="./stopL">
+                    <button class="button" type="submit">StopL</button>
+                  </form>
+                </td>
+                <td>
+                  <form action="./cw">
+                    <button class="button" type="submit">CW</button>
+                  </form>
+                </td>
+              </tr>
+            </table>
+            <br>
+            <form action="./down">
+              <button class="button" type="submit">Down</button>
+            </form>
+          </td>
+          <td>
+            <form action="./forward">
+              <button class="button" type="submit">Forward</button>
+            </form>
+            <br>
+            <table>
+              <tr>
+                <td>
+                  <form action="./clampClose">
+                    <button class="button" type="submit">ClampClose</button>
+                  </form>
+                </td>
+                <td>
+                  <form action="./stopR">
+                    <button class="button" type="submit">StopR</button>
+                  </form>
+                </td>
+                <td>
+                  <form action="./clampOpen">
+                    <button class="button" type="submit">clampOpen</button>
+                  </form>
+                </td>
+              </tr>
+            </table>
+            <br>
+            <form action="./backward">
+              <button class="button" type="submit">Backward</button>
+            </form>
+          </td>
+        </tr>
+      </table>
+    </center>
+  </body>
+</html>
 """
+
 
 
 # Listen for connections
@@ -110,21 +145,36 @@ while True:
         led.on()
         # print(request)
         
-        if b'GET /forward' in request:
+        if b'GET /up' in request:
             # forward 동작 수행
-            print("Input Command: forward")
-        elif b'GET /left' in request:
+            print("Input Command: Up")
+        elif b'GET /ccw' in request:
             # left 동작 수행
-            print("Input Command: left")
-        elif b'GET /right' in request:
+            print("Input Command: CCW")
+        elif b'GET /stopL' in request:
             # right 동작 수행
-            print("Input Command: right")
-        elif b'GET /stop' in request:
+            print("Input Command: StopL")
+        elif b'GET /cw' in request:
             # stop 동작 수행
-            print("Input Command: stop")
-        elif b'GET /back' in request:
+            print("Input Command: CW")
+        elif b'GET /down' in request:
             # back 동작 수행
-            print("Input Command: back")
+            print("Input Command: Down")
+        elif b'GET /forward' in request:
+            # back 동작 수행
+            print("Input Command: Forward")
+        elif b'GET /clampClose' in request:
+            # back 동작 수행
+            print("Input Command: ClampClose")
+        elif b'GET /stopR' in request:
+            # back 동작 수행
+            print("Input Command: StopR")
+        elif b'GET /clampOpen' in request:
+            # back 동작 수행
+            print("Input Command: ClampOpen")
+        elif b'GET /backward' in request:
+            # back 동작 수행
+            print("Input Command: Backward")
             
         cl.send('HTTP/1.0 200 OK\r\nContent-type: text/html\r\n\r\n')
         cl.send(html)
@@ -134,6 +184,5 @@ while True:
     except OSError as e:
         cl.close()
         print('connection closed')
-
-
+        s.close()
 

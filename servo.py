d7d36bc11d53751a35fc737550f42339cd0c1d53
Jason Juhyun Kim <mtinet@hanmail.net>
2023-10-15 11:44:48 +0900

    Update servo.py

--- servo.py
+++ servo.py
@@ -2,14 +2,8 @@ from time import sleep
 from machine import Pin, PWM
 
 # 서보 핀, 주파수 설정
-servo0 = PWM(Pin(16))
-servo0.freq(50)
-servo1 = PWM(Pin(17))
-servo1.freq(50)
-servo2 = PWM(Pin(18))
-servo2.freq(50)
-servo3 = PWM(Pin(19))
-servo3.freq(50)
+servo = PWM(Pin(0))
+servo.freq(50)
 
 # 서보 각도를 0~180도로 입력할 수 있도록 함수를 하나 만듬
 def setAngle(servoName, angle):
@@ -19,9 +13,14 @@ def setAngle(servoName, angle):
     
 # 반복동작 
 while True:
-    setAngle(servo0, 120)
-    setAngle(servo1, 90)
-    setAngle(servo2, 60)
-    setAngle(servo3, 60)
+    setAngle(servo,30)
+    sleep(1)
+    setAngle(servo,60)
+    sleep(1)
+    setAngle(servo,95)
+    sleep(1)
+
+
+
 
 
